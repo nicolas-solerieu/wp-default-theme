6721eda006dd2eaf737732ba05064a4396edc502
Nicolas Solerieu <solerieunicolas@gmail.com>
2016-02-13 18:15:57 +0100

    Admin menu removal function + logo change

--- custom/inc/custom-functions.php
+++ custom/inc/custom-functions.php
@@ -14,3 +14,23 @@ add_theme_support( 'post-thumbnails' );
  */
 add_post_type_support('page', 'excerpt');
 
+
+/**
+ * Removal of admin menu entries
+ */
+function remove_menus(){
+  
+  //remove_menu_page( 'index.php' );                  //Dashboard
+  //remove_menu_page( 'edit.php' );                   //Posts
+  //remove_menu_page( 'upload.php' );                 //Media
+  //remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  //remove_menu_page( 'themes.php' );                 //Appearance
+  //remove_menu_page( 'plugins.php' );                //Plugins
+  //remove_menu_page( 'users.php' );                  //Users
+  remove_menu_page( 'tools.php' );                  //Tools
+  //remove_menu_page( 'options-general.php' );        //Settings
+  
+}
+add_action( 'admin_menu', 'remove_menus' );
+
